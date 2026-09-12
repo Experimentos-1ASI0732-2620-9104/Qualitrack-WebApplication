93772bdf4eda97d295df80fd437634c63c251ec0
BJRM03 <ruizbilly320@gmail.com>
2026-09-12 18:32:04 -0500

    feat(app): bootstrap routing providers and environments

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authenticationInterceptor } from './iam/infrastructure/iam-interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptors([authenticationInterceptor])),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
+      fallbackLang: 'en_US',
+    }),
+    provideCharts(withDefaultRegisterables()),
+  ],
+};
